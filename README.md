4809ae2f96c0875b25283b64e2603be2519e3aa4
elizabeth campillo <elizabethcampillo15@gmail.com>
2018-11-14 19:41:10 -0500

    actualizacion de archivos

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # pruebaheroku
+
+## prueba de plataforma Heroku
+
+texto normal
---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Prueba heroku</title>
+</head>
+<body>
+<header>
+	<h1> Esto es una prueba heroku</h1>
+
+</header>
+<section>
+	<div>Este es un mensaje de prueba heroku</div>
+
+</section>
+
+<footer>
+	<ul>
+		<li>Direccion: </li>
+		<li>Telefono: </li>
+		<li>Email: </li>
+	</ul>
+</footer>
+
+</body>
+</html>
